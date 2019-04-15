e1e78fa026599e13afc7573bd58e134a916a74d9
frogxdeliver <33261077+frogxdeliver@users.noreply.github.com>
2019-04-15 09:55:38 -0500

    Josh's part

--- Tappity Tap/GameScene.swift
+++ Tappity Tap/GameScene.swift
@@ -14,13 +14,18 @@ class GameScene: SKScene {
     let cameraNode = SKCameraNode()
     let cameraMovePointsPerSec: CGFloat = 200.0
     let playableRect: CGRect
+    var cameraRect : CGRect {
+        let x = cameraNode.position.x - size.width/2 + (size.width - playableRect.width)/2
+        let y = cameraNode.position.y - size.height/2 + (size.height - playableRect.height)/2
+        return CGRect(x: x, y: y, width: playableRect.width, height: playableRect.height)
+    }
     
     
     override func didMove(to view: SKView) {
         //16:9 aspect ratio
         /*let maxAspectRatio: CGFloat = 16.0/9.0
-        let maxAspectRatioHeight = size.width / maxAspectRatio
-        let playableMargin: CGFloat = (size.height - maxAspectRatioHeight)/2*/
+         let maxAspectRatioHeight = size.width / maxAspectRatio
+         let playableMargin: CGFloat = (size.height - maxAspectRatioHeight)/2*/
         
         for i in 0...1 {
             let background = backgroundNode()
@@ -42,15 +47,15 @@ class GameScene: SKScene {
                               width: size.width,
                               height: playableHeight)
         /*var textures: [SKTexture] = []
-        //animates the zombie with an array looping through different images
-        for i in 1...4{
-            textures.append(SKTexture(imageNamed: ":zombie\(i)"))
-        }
-        textures.append(textures[2])
-        textures.append(textures[1])
-        
-        zombieAnimation = SKAction.animate(with: textures,
-                                           timePerFrame: 0.1)*/
+         //animates the zombie with an array looping through different images
+         for i in 1...4{
+         textures.append(SKTexture(imageNamed: ":zombie\(i)"))
+         }
+         textures.append(textures[2])
+         textures.append(textures[1])
+         
+         zombieAnimation = SKAction.animate(with: textures,
+         timePerFrame: 0.1)*/
         super.init(size: size)
     }
     required init(coder aDecoder: NSCoder){
@@ -85,24 +90,75 @@ class GameScene: SKScene {
         return backgroundNode
     }
     
-    /*
-    //calculates the amount the camera needs to move
-    func moveCamera() {
-        let backgroundVelocity =
-            CGPoint(x: cameraMovePointsPerSec, y: 0)
-        let amountToMove = backgroundVelocity * CGFloat(dt)
-        cameraNode.position += amountToMove
+    func floorNode() -> SKSpriteNode {
+        let floorNode = SKSpriteNode()
+        floorNode.anchorPoint = CGPoint.zero
+        floorNode.name = "floor"
+        
+        let floor1 = SKSpriteNode(imageNamed: "Jungle_Floor_1")
+        floor1.anchorPoint = CGPoint.zero
+        //Whereever the player is
+        //floor1.position = CGPoint
+        floorNode.addChild(floor1)
+        
+        let floor2 = SKSpriteNode(imageNamed: "Jungle_Floor_2")
+        floor2.anchorPoint = CGPoint.zero
+        floor2.position =
+            CGPoint(x: floor1.size.width, y:0)
+        floorNode.addChild(floor2)
+        
+        
+        floorNode.size = CGSize(
+            width: floor1.size.width + floor2.size.width,
+            height: floor1.size.height)
+        return floorNode
+        
     }
     
-    //calculates the current visible playable area
-    var cameraRect : CGRect{
-        let x = cameraNode.position.x - size.width/2
-            + (size.width - playableRect.width)/2
-        let y = cameraNode.position.y - size.height/2
-            + (size.height - playableRect.height)/2
-        return CGRect(x: x,
-                      y: y,
-                      width: playableRect.width,
-                      height: playableRect.height)
-    }*/
+    func moveCamera(){
+        let backgroundVelocity = CGPoint(x: cameraMovePointsPerSec, y: 0)
+        let amountToMove = backgroundVelocity * CGFloat(dt)
+        cameraNode.position += amountToMove
+        
+        enumerateChildNodes(withName: "background") { node, _ in
+            let background = node as! SKSpriteNode
+            if background.position.x + background.size.width < self.cameraRect.origin.x {
+                background.position = CGPoint(x: background.position.x + background.size.width*2, y: background.position.y)
+            }
+        }
+    }
+    
+    /*
+     //calculates the amount the camera needs to move
+     func moveCamera() {
+     let backgroundVelocity =
+     CGPoint(x: cameraMovePointsPerSec, y: 0)
+     let amountToMove = backgroundVelocity * CGFloat(dt)
+     cameraNode.position += amountToMove
+     }
+     
+     //calculates the current visible playable area
+     var cameraRect : CGRect{
+     let x = cameraNode.position.x - size.width/2
+     + (size.width - playableRect.width)/2
+     let y = cameraNode.position.y - size.height/2
+     + (size.height - playableRect.height)/2
+     return CGRect(x: x,
+     y: y,
+     width: playableRect.width,
+     height: playableRect.height)
+     }*/
+    
+    func spawnCoin(){
+        let coin = SKSpriteNode(imageNamed: "coin")
+        coin.name = "coin"
+        coin.position = CGPoint(
+            x: cameraRect.maxX + coin.size.width/2,
+            y: CGFloat.random(
+                min: cameraRect.minY + coin.size.height/2,
+                max: cameraRect.maxY - coin.size.height/2))
+        addChild(coin)
+    }
+    
+    
 }
